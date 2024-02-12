34c6658e2bb4b4b6ec6ed377878e23c5c73d1f41
silvawillian <willian.j.silva@gmail.com>
2024-02-11 21:48:22 -0300

    make endpoints functional

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,14 @@ dotenvFlow.config()
 const app: Express = express()
 const port = process.env.PORT ?? 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
 app.use('/clientes', clientes)
-
 const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
--- src/routes/clientes.ts
+++ src/routes/clientes.ts
@@ -4,15 +4,31 @@ import express from 'express'
 import db from '../database'
 const router = express.Router()
 
+interface Cliente {
+  id: number
+  nome: string
+  limite: number
+  saldo: number
+}
+
+interface Transacao {
+  id: number
+  tipo: 'c' | 'd'
+  descricao: string
+  valor: number
+  cliente_id: Cliente['id']
+  realizada_em: Date
+}
+
 router.get('/:id/extrato', async (req, res) => {
   const { id } = req.params
 
-  const clienteQuery = db`
+  const clienteQuery = db<Cliente[]>`
     SELECT * FROM clientes c WHERE c.id = ${id}
   `
 
-  const transacoesQuery = db`
-    SELECT * FROM transacoes t WHERE t.id = ${id}
+  const transacoesQuery = db<Transacao[]>`
+    SELECT * FROM transacoes t WHERE t.cliente_id = ${id}
   `
 
   const [[cliente], transacoes] = await Promise.all([
@@ -20,12 +36,11 @@ router.get('/:id/extrato', async (req, res) => {
     transacoesQuery
   ])
 
-  console.log(cliente)
+  if (cliente === undefined) {
+    return res.status(404).send({ msg: `cliente ${id} nao encontrado!` })
+  }
+
   console.log(transacoes)
-
-  if (cliente.length === 0) return res.status(404).send({})
-
-  console.log(cliente)
 
   return res.status(200).send({
     saldo: {
@@ -42,12 +57,49 @@ router.get('/:id/extrato', async (req, res) => {
   })
 })
 
-router.post('/:id/transacoes', (req, res) => {
-  console.log(req.params.id)
+router.post('/:id/transacoes', async (req, res) => {
+  const { body } = req
+  const { id } = req.params
 
-  res.status(200).send({
-    limite: 10000,
-    saldo: -9098
+  const [cliente] = await db<Cliente[]>`
+    SELECT * FROM clientes c WHERE c.id = ${id}
+  `
+
+  if (cliente === undefined) {
+    return res.status(404).send({ msg: `cliente ${id} nao encontrado!` })
+  }
+
+  const transacao = {
+    cliente_id: id,
+    tipo: body.tipo,
+    descricao: body.descricao,
+    realizada_em: new Date(),
+    valor: body.valor
+  }
+
+  let novoSaldo = 0
+
+  if (body.tipo === 'c') {
+    novoSaldo = cliente.saldo + body.valor
+    await db`UPDATE clientes SET saldo = ${novoSaldo} WHERE id = ${id}`
+  }
+
+  if (body.tipo === 'd') {
+    novoSaldo = cliente.saldo - body.valor
+    console.log(novoSaldo)
+    if (novoSaldo + cliente.limite < 0) {
+      return res
+        .status(422)
+        .send({ msg: 'Operação não permitida, saldo insuficiente...' })
+    }
+  }
+
+  await db`INSERT INTO transacoes (cliente_id, tipo, descricao, realizada_em, valor) VALUES (${transacao.cliente_id}, ${transacao.tipo}, ${transacao.descricao}, ${transacao.realizada_em}, ${transacao.valor})`
+  await db`UPDATE clientes SET saldo = ${novoSaldo} WHERE id = ${id}`
+
+  return res.status(200).send({
+    limite: cliente.limite,
+    saldo: novoSaldo
   })
 })
 
